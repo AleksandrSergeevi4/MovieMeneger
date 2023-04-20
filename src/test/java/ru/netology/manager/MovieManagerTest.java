90023bb9c6ad8ece7c3effca29efe5dd2434bf38
Alex.Sn <alex7777@nxt.ru>
2023-04-21 01:10:54 +0300

    Change files

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -6,11 +6,11 @@ import ru.netology.domain.Movie;
 
 public class MovieManagerTest {
     Movie movie1 = new Movie(1, "Бладшот", "боевик");
-    Movie movie2 = new Movie(2, "Вперед", "мультфилм");
+    Movie movie2 = new Movie(2, "Вперед", "мультфильм");
     Movie movie3 = new Movie(3, "Отель <<Белград>>", "комедия");
     Movie movie4 = new Movie(4, "Джентльмены", "боевик");
     Movie movie5 = new Movie(5, "Человек-невидимка", "ужасы");
-    Movie movie6 = new Movie(6, "Тролли. Мировой тур", "мультфиль");
+    Movie movie6 = new Movie(6, "Тролли. Мировой тур", "мультфильм");
     Movie movie7 = new Movie(7, "Номер один", "комедия");
 
     @Test
